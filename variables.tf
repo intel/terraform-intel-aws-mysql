5fb4f3e1f73cce00662c64527848bdc1a7270aa5
Kinder Wischmeier <kinder.wischmeier@intel.com>
2022-10-18 08:07:03 -0700

    Added variable parameter and added parameters

--- variables.tf
+++ variables.tf
@@ -6,10 +6,10 @@ variable "mysql_server_name" {
 variable "allocated_storage" {
   type        = string
   description = "The allocated storage in gibibytes."
-  default = 400
+  default     = 400
 }
 variable "engine" {
-  type = string
+  type    = string
   default = "mysql"
 }
 # we recommend memory optimized instances - db.m61.large, db.m6i.xlarge, db.m6i.2xlarge, db.m6i.4xlarge, db.m6i.8xlarge, db.m6i.12xlarge, db.m6i.16xlarge, db.m6i.24xlarge, db.m6i.32xlarge
@@ -18,7 +18,7 @@ variable "engine" {
 variable "instance_class" {
   type        = string
   description = "The instance type of the RDS instance."
-  default = "db.m6i.8xlarge"
+  default     = "db.m6i.8xlarge"
 }
 variable "availability_zone" {
   description = "The Availability Zone of the RDS instance"
@@ -53,6 +53,181 @@ variable "final_snapshot_identifier_prefix" {
 }
 variable "max_connections" {
   description = "Max connections"
-  type = number
-  default = 4000
+  type        = number
+  default     = 4000
+}
+variable "family" {
+  type    = string
+  default = "mysql8.0"
+}
+variable "parameters" {
+  description = "A list of DB parameter maps to apply"
+  type        = list(map(string))
+  default = [
+    {
+      name  = "table_open_cache"
+      value = 8000
+    },
+    {
+      name         = "table_open_cache_instances"
+      value        = 16
+      apply_method = "pending-reboot"
+    },
+    {
+      name  = "max_connections"
+      value = 4000
+    },
+    {
+      name         = "back_log"
+      value        = 1500
+      apply_method = "pending-reboot"
+    },
+    {
+      name  = "default_password_lifetime"
+      value = 0
+    },
+    {
+      name         = "performance_schema"
+      value        = "OFF"
+      apply_method = "pending-reboot"
+    },
+    {
+      name  = "max_prepared_stmt_count"
+      value = 128000
+    },
+    {
+      name  = "character_set_server"
+      value = "latin1"
+    },
+    {
+      name  = "collation_server"
+      value = "latin1_swedish_ci"
+    },
+    {
+      name  = "transaction_isolation"
+      value = "REPEATABLE-READ"
+    },
+    {
+      name         = "innodb_log_file_size"
+      value        = 1024 * 1024 * 1024
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "innodb_open_files"
+      value        = 4000
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "innodb_buffer_pool_instances"
+      value        = 16
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "innodb_log_buffer_size"
+      value        = 67108864
+      apply_method = "pending-reboot"
+    },
+    {
+      name  = "innodb_thread_concurrency"
+      value = 0
+    },
+    {
+      name  = "innodb_flush_log_at_trx_commit"
+      value = 0
+    },
+    {
+      name  = "innodb_max_dirty_pages_pct"
+      value = 90
+    },
+    {
+      name  = "innodb_max_dirty_pages_pct_lwm"
+      value = 10
+    },
+    {
+      name  = "join_buffer_size"
+      value = 32 * 1024
+    },
+    {
+      name  = "sort_buffer_size"
+      value = 32 * 1024
+    },
+    {
+      name         = "innodb_use_native_aio"
+      value        = 1
+      apply_method = "pending-reboot"
+    },
+    {
+      name  = "innodb_stats_persistent"
+      value = 1
+    },
+    {
+      name  = "innodb_spin_wait_delay"
+      value = 6
+    },
+    {
+      name  = "innodb_max_purge_lag_delay"
+      value = 300000
+    },
+    {
+      name  = "innodb_max_purge_lag"
+      value = 0
+    },
+    {
+      name  = "innodb_checksum_algorithm"
+      value = "none"
+    },
+    {
+      name  = "innodb_io_capacity"
+      value = 4000
+    },
+    {
+      name  = "innodb_io_capacity_max"
+      value = 20000
+    },
+    {
+      name  = "innodb_lru_scan_depth"
+      value = 9000
+    },
+    {
+      name  = "innodb_change_buffering"
+      value = "none"
+    },
+    {
+      name         = "innodb_page_cleaners"
+      value        = 4
+      apply_method = "pending-reboot"
+    },
+    {
+      name  = "innodb_undo_log_truncate"
+      value = "off"
+    },
+    {
+      name  = "innodb_adaptive_flushing"
+      value = 1
+    },
+    {
+      name  = "innodb_flush_neighbors"
+      value = 0
+    },
+    {
+      name         = "innodb_read_io_threads"
+      value        = 16
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "innodb_write_io_threads"
+      value        = 16
+      apply_method = "pending-reboot"
+    },
+    {
+      name         = "innodb_purge_threads"
+      value        = 4
+      apply_method = "pending-reboot"
+    },
+    {
+      name  = "innodb_adaptive_hash_index"
+      value = 0
+    }
+
+  ]
 }
