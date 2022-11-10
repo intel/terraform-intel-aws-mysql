14937e36717052beef760dbf3c932d37267ac501
kalenarndt <kalenarndt@gmail.com>
2022-11-10 14:41:29 -0700

    terraform format and output changes

--- examples/intel-optimized-mysql-server/outputs.tf
+++ examples/intel-optimized-mysql-server/outputs.tf
@@ -1,24 +1,27 @@
 output "id" {
-  value       = module.optimized-mysql-server.id
+  value       = module.optimized-mysql-server.mysql_id
   description = "Instance ID for the MySQL instance that has been created"
 }
 
 output "address" {
-  value       = module.optimized-mysql-server.address
+  value       = module.optimized-mysql-server.mysql_address
   description = "Hostname of the MySQL instance that has been created"
+  sensitive   = true
 }
 
 output "port" {
-  value       = module.optimized-mysql-server.port
+  value       = module.optimized-mysql-server.mysql_port
   description = "Port that the MySQL instance is listening on"
 }
 
 output "username" {
-  value       = module.optimized-mysql-server.username
+  value       = module.optimized-mysql-server.mysql_username
   description = "Master username for the MySQL instance that has been created"
+  sensitive   = true
 }
 
 output "endpoint" {
-  value       = module.optimized-mysql-server.endpoint
+  value       = module.optimized-mysql-server.mysql_endpoint
   description = "Connection endpoing for the MySQL instance that has been created"
+  sensitive   = true
 }
--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,27 @@
-output "id" {
+output "mysql_id" {
   value       = aws_db_instance.mysql_server.id
   description = "Instance ID for the MySQL instance that has been created"
 }
 
-output "address" {
+output "mysql_address" {
   value       = aws_db_instance.mysql_server.address
   description = "Hostname of the MySQL instance that has been created"
+  sensitive   = true
 }
 
-output "port" {
+output "mysql_port" {
   value       = aws_db_instance.mysql_server.port
   description = "Port that the MySQL instance is listening on"
 }
 
-output "username" {
+output "mysql_username" {
   value       = aws_db_instance.mysql_server.username
   description = "Master username for the MySQL instance that has been created"
+  sensitive   = true
 }
 
-output "endpoint" {
+output "mysql_endpoint" {
   value       = aws_db_instance.mysql_server.endpoint
-  description = "Connection endpoing for the MySQL instance that has been created"
+  description = "Connection endpoint for the MySQL instance that has been created"
+  sensitive   = true
 }
