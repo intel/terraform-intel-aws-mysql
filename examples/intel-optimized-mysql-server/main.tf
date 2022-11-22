b58f0f64c82993316af60c636af5907b2c048300
Kalen Arndt <kalenarndt@gmail.com>
2022-11-22 12:47:16 -0700

    Module Refactor
    * refactor and mysql testing
    
    * add variables
    
    * add pre-condition
    
    * copy paste error on variables
    
    * add missing values from azure mysql module
    
    * fix variable ref issue on 108
    
    * found unused vars and missing type constraints
    
    * 1.3.5 logic fixes and increased default
    
    * 1.3.5 logic fixes and increased default
    
    * fix input param on example module
    
    * fix defaults per lucas
    
    * change defaults
    
    * add description to db_parameters map
    
    * example changes

--- examples/intel-optimized-mysql-server/main.tf
+++ examples/intel-optimized-mysql-server/main.tf
@@ -6,9 +6,9 @@
 module "optimized-mysql-server" {
   source      = "../../"
   db_password = var.db_password
+  rds_identifier = "<NAME-FOR-RDS-INSTANCE>"
   # Update the vpc_id below for the VPC that this module will use. Find the vpc-id in your AWS account
   # from the AWS console or using CLI commands. In your AWS account, the vpc-id is represented as "vpc-",
   # followed by a set of alphanumeric characters. One sample representation of a vpc-id is vpc-0a6734z932p20c2m4
-  vpc_id                      = "vpc-0ae643f51f9761b2f"
-  aws_database_instance_class = "db.m6i.2xlarge"
+  vpc_id = "<YOUR-VPC-ID-HERE>"
 }
--- versions.tf
+++ versions.tf
@@ -1,9 +1,13 @@
 terraform {
-  required_version = ">=1.2.0"
+  required_version = ">=1.3.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.36.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.4.3"
     }
   }
 }
